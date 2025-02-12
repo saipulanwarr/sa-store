37f4a78965caa7841cbb9598920dc0bbaf52afaa
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-12 14:35:31 +0700

    added product component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrival"
+        limit={10}
+      />
+    </>
+  );
 }
 
 export default HomePage;
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+function ProductPrice({
+  value,
+  className,
+}: {
+  value: number;
+  className?: string;
+}) {
+  const stringValue = value.toFixed(3);
+  const [intValue, floatValue] = stringValue.split(".");
+
+  return (
+    <p className={cn("text-2xl", className)}>
+      <span className="text-xs align-super">Rp.</span>
+      {intValue}
+      <span className="text-xs align-super">. {floatValue}</span>
+    </p>
+  );
+}
+
+export default ProductPrice;
