a2b5649af865ea3e7265eca19d7a470571b98a33
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-12 11:33:32 +0700

    added sheet menu

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
-import { Button } from "@/components/ui/button";
-import { ShoppingCart, UserIcon } from "lucide-react";
-import ModeToggle from "./mode-toggle";
+import Menu from "./menu";
 
 function Header() {
   return (
@@ -24,19 +22,7 @@ function Header() {
             </span>
           </Link>
         </div>
-        <div className="space-x-2">
-          <ModeToggle />
-          <Button asChild variant="ghost">
-            <Link href="/cart">
-              <ShoppingCart /> Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-in">
-              <UserIcon /> Sign In
-            </Link>
-          </Button>
-        </div>
+        <Menu />
       </div>
     </header>
   );
