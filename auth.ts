c5019acafa7ec3a2ea7512f980d165268682f41d
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-20 21:25:08 +0700

    added orders page

--- auth.ts
+++ auth.ts
@@ -92,36 +92,47 @@ export const config = {
           
             return token;
         },
-        authorized({request, auth}: any){
+        authorized({ request, auth }: any) {
+            // Array of regex patterns of paths we want to protect
             const protectedPaths = [
-                /\/shipping-address/,
-                /\/payment-method/,
-                /\/place-order/,
-                /\/profile/,
-                /\/user\/(.*)/,
-                /\/order\/(.*)/,
-                /\/admin/,
+              /\/shipping-address/,
+              /\/payment-method/,
+              /\/place-order/,
+              /\/profile/,
+              /\/user\/(.*)/,
+              /\/order\/(.*)/,
+              /\/admin/,
             ];
-
-            const {pathname} = request.nextUrl;
-
-            if(!request.cookies.get('sessionCartId')){
-                const sessionCartId = crypto.randomUUID();
-                const newRequestHeaders = new Headers(request.headers);
-
-                const response = NextResponse.next({
-                    request: {
-                        headers: newRequestHeaders
-                    }
-                })
-
-                response.cookies.set('sessionCartId', sessionCartId);
-
-                return response;
-            }else{
-                return true;
+      
+            // Get pathname from the req URL object
+            const { pathname } = request.nextUrl;
+      
+            // Check if user is not authenticated and accessing a protected path
+            if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+      
+            // Check for session cart cookie
+            if (!request.cookies.get('sessionCartId')) {
+              // Generate new session cart id cookie
+              const sessionCartId = crypto.randomUUID();
+      
+              // Clone the req headers
+              const newRequestHeaders = new Headers(request.headers);
+      
+              // Create new response and add the new headers
+              const response = NextResponse.next({
+                request: {
+                  headers: newRequestHeaders,
+                },
+              });
+      
+              // Set newly generated sessionCartId in the response cookies
+              response.cookies.set('sessionCartId', sessionCartId);
+      
+              return response;
+            } else {
+              return true;
             }
-        }
+        },
     },
 } satisfies NextAuthConfig;
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import qs from 'query-string';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
@@ -97,3 +98,28 @@ export const formatDateTime = (dateString: Date) => {
     timeOnly: formattedTime,
   };
 };
+
+export function formUrlQuery({
+  params,
+  key,
+  value,
+}: {
+  params: string;
+  key: string;
+  value: string | null;
+}){
+  const query = qs.parse(params)
+
+  query[key] = value;
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query,
+    },
+    {
+      skipNull: true,
+    }
+  )
+
+}
