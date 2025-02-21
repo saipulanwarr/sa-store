b02188115e123067bff5258def0e700c985a86e0
saipulanwarr <saipulanwar180994@gmail.com>
2025-02-21 11:02:26 +0700

    added update profile page

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,7 +1,24 @@
+import { auth } from "@/auth";
+import { Metadata } from "next";
 import React from "react";
+import { SessionProvider } from "next-auth/react";
+import ProfileForm from "./profile-form";
 
-function ProfilePage() {
-  return <div>ProfilePage</div>;
+export const metadata: Metadata = {
+  title: "Customer Profile",
+};
+
+async function ProfilePage() {
+  const session = await auth();
+
+  return (
+    <SessionProvider session={session}>
+      <div className="max-w-md mx-auto space-y-4">
+        <h2 className="h2-bold">Profile</h2>
+        <ProfileForm />
+      </div>
+    </SessionProvider>
+  );
 }
 
 export default ProfilePage;
--- auth.ts
+++ auth.ts
@@ -89,6 +89,10 @@ export const config = {
                 }
               }
             }
+
+            if (session?.user.name && trigger === 'update') {
+              token.name = session.user.name;
+            }
           
             return token;
         },
